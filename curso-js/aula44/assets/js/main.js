14bc4c5a0aa05695ebdaa8700344cacc9ab233d5
Matheus Moraes <matheus1999moraes@gmail.com>
2023-06-30 15:32:59 -0300

    atvd

--- curso-js/aula44/assets/js/main.js
+++ curso-js/aula44/assets/js/main.js
@@ -1,7 +1,7 @@
 // pegando os elementos do html
 const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
-const tarefas = document.querySelector('.tarefa');
+const tarefas = document.querySelector('.tarefas');
 
 
 function criaLi() {
@@ -11,11 +11,70 @@ function criaLi() {
 
 function criaTarefa(textoInput) {
     const li = criaLi();
-    li.innerHTML = textoInput;
+    li.innerText = textoInput;
     tarefas.appendChild(li);
+    limpaInput();
+    criaBtnApagar(li);
+    salvarTarefas();
 }
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBtnApagar(li) {
+    li.innerText += ' ';
+    const btnApagar = document.createElement('button');
+    btnApagar.innerText = 'Apagar';
+    li.appendChild(btnApagar);
+
+    btnApagar.setAttribute('class', 'apagar');
+    btnApagar.setAttribute('title', 'Apagar essa tarefa');
+}
+
+inputTarefa.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
 
 btnTarefa.addEventListener('click', function() {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+});
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+    console.log(listaDeTarefas);
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
---
+++ curso-js/aula45/idnex.js
@@ -0,0 +1,31 @@
+// Declaração de função (Function hoisting)
+falaOi();
+function falaOi() {
+    console.log('Oie');
+}
+
+// First-class objects (Objetos de primeira classe)
+const souUmDado = function() {
+    console.log('Sou um dado.');
+};
+souUmDado();
+
+function executaFuncao(funcao) {
+    console.log('Vou executar sua função abaixo: ');
+    funcao();
+}
+executaFuncao(souUmDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro de um objeto
+const obj = {
+    falar: function() {
+        console.log('Estou falando...')
+    }
+};
+obj.falar();
